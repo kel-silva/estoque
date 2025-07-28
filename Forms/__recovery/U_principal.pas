57246c0c9ebff1a5dc2a30890c0af10ac224bb16
kel-silva <kellogical1@gmail.com>
2025-07-28 14:33:47 -0300

    aula 09 criando conexao banco de dados e form padrao

--- Forms/__recovery/U_principal.pas
+++ Forms/__recovery/U_principal.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons, Vcl.ComCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons, Vcl.ComCtrls,
+  Vcl.Menus;
 
 type
   TFrm_principal = class(TForm)
